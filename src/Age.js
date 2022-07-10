137e71316d7a8d528dc6642797ceecd8a8a085ec
Martib01 <martybb01@gmail.com>
2022-07-10 17:22:39 +0200

    double condition in age prop

--- src/Age.js
+++ src/Age.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export class Age extends React.Component {
   render() {
-    return <div>{!!this.props.age && <p>Your age is {this.props.age}</p>}</div>;
+    return (
+      <div>
+        {this.props.age > 18 && this.props.age < 65 && (
+          <p>Your age is {this.props.age}</p>
+        )}
+      </div>
+    );
   }
 }
