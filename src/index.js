2b32342231f67c05e406efc8342e0d1bb4cd55b7
Martib01 <martybb01@gmail.com>
2022-07-10 11:15:49 +0200

    Q&A

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import { App } from "./App";
 const root = createRoot(document.getElementById("root"));
 
 root.render(<App />);
+
+/* What happens if no name prop is passed to the Welcome component? 
+- if no name prop is passed then we'll get undefined
+Can you set a default value for the name prop? 
+- The logical operator OR (||) is used to set a fallback value for the name prop whenever it is not passed.
+    <p>Welcome, {this.props.name} || "Luca"</p>
+*/
