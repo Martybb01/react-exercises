e393736045f51f5bcd889f9f42f8cf9a2240cf97
Martib01 <martybb01@gmail.com>
2022-07-10 11:22:10 +0200

    Add age prop to Welcome component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name="marti" />
+        <Welcome name="marti" age={21} />
       </div>
     );
   }
