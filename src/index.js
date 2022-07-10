bb11b54b5c2356eca600ec2bd2c2257fe2a3260c
Martib01 <martybb01@gmail.com>
2022-07-10 11:40:39 +0200

    Q&A + modify name prop value

--- src/index.js
+++ src/index.js
@@ -11,3 +11,8 @@ Can you set a default value for the name prop?
 - The logical operator OR (||) is used to set a fallback value for the name prop whenever it is not passed.
     <p>Welcome, {this.props.name} || "Luca"</p>
 */
+
+/* What happens if the name prop is a JSX expression instead of a string?
+How do you pass a prop that contains a JSX expression?
+- You can pass any JS expression as a prop, by surrounding it with {}
+*/
