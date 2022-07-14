eb0d65d01cde78cabbd91a2238b3311a1cd58f96
Martib01 <martybb01@gmail.com>
2022-07-14 15:10:31 +0200

    inline if-else

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name="John" age={21} />
+        <Welcome name="John" age={16} />
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -7,8 +7,10 @@ export class Welcome extends React.Component {
       <div>
         <p>
           Welcome, {this.props.name === "John" && this.props.name}!
-          {this.props.age && this.props.age > 18 && this.props.age < 65 && (
+          {this.props.age > 18 ? (
             <Age age={this.props.age} />
+          ) : (
+            <p>You're very young</p>
           )}
         </p>
       </div>
