2a899e76b48040d815b64d91846f8a40e4fda523
Martib01 <martybb01@gmail.com>
2022-07-20 18:23:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Clock } from "../../my-app/src/Clock";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
-import { Counter } from "../../my-app/src/Counter";
 
 export class App extends React.Component {
   render() {
