5322457c663388a2bad2a7f3a6bb4f66ab880f0e
Martib01 <martybb01@gmail.com>
2022-07-23 13:25:24 +0200

    lifecycle methods

--- src/Counter.js
+++ src/Counter.js
@@ -4,6 +4,7 @@ import { CounterDisplay } from "./CounterDisplay";
 export class Counter extends React.Component {
   state = {
     count: this.props.initialValue,
+    update: 0,
   };
   // constructor(props) {
   //   super(props);
@@ -16,10 +17,44 @@ export class Counter extends React.Component {
   //   }, this.props.timeout);
   // }
   componentDidMount() {
-    this.timerID = setInterval(() => this.tickTok(), this.props.timeout);
+    this.timerID = setInterval(() => {
+      this.tickTok();
+      this.setState((state) => {
+        return {
+          update: state.update + 1,
+        };
+      });
+    }, this.props.timeout);
   }
 
+  componentDidUpdate(props, state) {
+    if (state.update === 10) {
+      this.setState(() => {
+        return {
+          count: this.props.initialValue,
+          update: 0,
+        };
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timerID) {
+      clearInterval(this.timerID);
+    }
+  }
   tickTok() {
+    // this.setState((state) => {
+    //   if (state.count < this.props.initialValue * 10) {
+    //     return {
+    //       count: state.count + this.props.incrementValue,
+    //     };
+    //   } else {
+    //     return {
+    //       count: this.props.initialValue,
+    //     };
+    //   }
+    // });
     this.setState((state) => {
       return {
         count: state.count + this.props.incrementValue,
