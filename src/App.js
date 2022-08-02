409c5b26f4e589918585860cb2eec36479aa8295
Martib01 <martybb01@gmail.com>
2022-08-02 11:18:20 +0200

    End context-01

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Container } from "./Container";
 import { Counter } from "./Counter";
+import { DisplayLanguage } from "./DisplayLanguage";
 import { Hello } from "./Hello";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
+import { LanguageContext } from "./LanguageContext";
 import { TodoList } from "./TodoList";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
+  state = {
+    language: "en",
+  };
+  handleLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
   render() {
     return (
       <Container title={<h2>React</h2>}>
+        <LanguageContext.Provider>
+          <select
+            value={this.state.language}
+            onChange={this.handleLanguageChange}
+          >
+            <option value="en">English</option>
+            <option value="it">Italiano</option>
+          </select>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
         <Hello />
         <Welcome name="marti" />
         <Age age={21} />
