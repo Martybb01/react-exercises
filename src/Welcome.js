4a61b619c5b4299efa69d4f0dadc1690c878507a
Martib01 <martybb01@gmail.com>
2022-08-03 17:31:43 +0200

    Welcome as a function component

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export class Welcome extends React.Component {
-  render() {
-    return (
-      <div className="welcome">
-        <p>Welcome, {this.props.name}!</p>
-      </div>
-    );
-  }
+export function Welcome(props) {
+  return (
+    <div>
+      <p>Welcome, {props.name}!</p>
+    </div>
+  );
 }
+
+// export class Welcome extends React.Component {
+//   render() {
+//     return (
+//       <div className="welcome">
+//         <p>Welcome, {this.props.name}!</p>
+//       </div>
+//     );
+//   }
+// }
