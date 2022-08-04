aa8bf4f9c172c8dc8aa535477cbf8fd37c24b6c6
Martib01 <martybb01@gmail.com>
2022-08-04 09:43:37 +0200

    End useState-01 / ClickCounter component

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class ClickCounter extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
-  state = {
-    count: this.props.initialValue,
+export function ClickCounter({ initialValue = 0 }) {
+  const [counter, setCounter] = useState(initialValue);
+
+  const handleCounterIncrement = () => {
+    setCounter((c) => c + 1);
   };
-  handleClickEvent = () => {
-    this.setState((state) => {
-      return {
-        count: state.count + this.props.incrementBy,
-      };
-    });
+
+  const CounterStyle = {
+    color: counter < 5 ? "red" : "green",
   };
-  render() {
-    const CounterStyle = {
-      color: this.state.count < 5 ? "red" : "green",
-    };
-    return (
-      <div style={CounterStyle}>
-        <p>{this.state.count}</p>
-        <button onClick={this.handleClickEvent}>Increment</button>
-      </div>
-    );
-  }
+
+  return (
+    <div style={CounterStyle}>
+      <p> {counter} </p>
+      <button onClick={handleCounterIncrement}>Increment</button>
+    </div>
+  );
 }
+
+// export class ClickCounter extends React.Component {
+//   //   constructor(props) {
+//   //     super(props);
+//   //   }
+//   state = {
+//     count: this.props.initialValue,
+//   };
+//   handleClickEvent = () => {
+//     this.setState((state) => {
+//       return {
+//         count: state.count + this.props.incrementBy,
+//       };
+//     });
+//   };
+//   render() {
+//     const CounterStyle = {
+//       color: this.state.count < 5 ? "red" : "green",
+//     };
+//     return (
+//       <div style={CounterStyle}>
+//         <p>{this.state.count}</p>
+//         <button onClick={this.handleClickEvent}>Increment</button>
+//       </div>
+//     );
+//   }
+// }
