3175acbe251a2cd3ef844b423fe59716f7ddaebe
Martib01 <martybb01@gmail.com>
2022-08-04 13:04:19 +0200

    make TodoList a function component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from "react";
-import { Age } from "./Age";
-import { ClickCounter } from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
 import { Container } from "./Container";
-import { Counter } from "./Counter";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { Hello } from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { LanguageContext } from "./LanguageContext";
 import { TodoList } from "./TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { Welcome } from "./Welcome";
-import { Sum } from "./Sum";
 
 export class App extends React.Component {
   state = {
@@ -26,51 +14,7 @@ export class App extends React.Component {
   render() {
     return (
       <Container title={<h2>React</h2>}>
-        <LanguageContext.Provider value={this.state.language}>
-          <select
-            value={this.state.language}
-            onChange={this.handleLanguageChange}
-          >
-            <option value="en">English</option>
-            <option value="it">Italiano</option>
-          </select>
-          <DisplayLanguage />
-        </LanguageContext.Provider>
-        <Hello />
-        <Welcome name="marti" />
-        <Age age={21} />
-        <Counter />
-        <ClickCounter initialValue={0} incrementBy={1} />
-        <ClickTracker />
-        <InteractiveWelcome />
-        <Login />
-        <UncontrolledLogin />
-        <TodoList
-          render={(items) => {
-            return (
-              <div>
-                <ul>
-                  {items.map((item, index) => (
-                    <li key={item + index}>
-                      {item}
-                      <button
-                        onClick={() => {
-                          items.splice(index, 1);
-                          this.setState({
-                            items: items,
-                          });
-                        }}
-                      >
-                        Remove Item
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            );
-          }}
-        ></TodoList>
-        <Sum />
+        <TodoList />
       </Container>
     );
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,43 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class TodoList extends React.Component {
-  state = {
+export function TodoList() {
+  const [data, setData] = useState({
     items: ["Squirtle", "Charmander", "Bulbasaur"],
     itemsInput: "",
-  };
-  handlerAdd = () => {
-    this.setState({
-      items: [...this.state.items, this.state.itemsInput],
+  });
+
+  function handleInputChange(event) {
+    setData((data) => {
+      return {
+        ...data,
+        itemsInput: event.target.value,
+      };
+    });
+  }
+
+  function handleAdd() {
+    setData({
+      items: [...data.items, data.itemsInput],
       itemsInput: "",
     });
-  };
-  handleInputChange = (event) => {
-    this.setState({
-      itemsInput: event.target.value,
-    });
-  };
-  handleReset = () => {
-    this.setState({
+  }
+  function handleReset() {
+    setData({
       items: [],
     });
-  };
-
-  render() {
-    return (
-      <div>
-        <div>{this.props.render(this.state.items)}</div>
-        <input
-          type="text"
-          name="username"
-          placeholder="please insert a name"
-          value={this.state.itemsInput}
-          onChange={this.handleInputChange}
-        ></input>
-        <button onClick={this.handlerAdd} disabled={!this.state.itemsInput}>
-          ADD
-        </button>
-        <button onClick={this.handleReset}>RESET</button>
-      </div>
-    );
   }
+  return (
+    <div>
+      <ul>
+        {data.items.map((item, index) => (
+          <li key={item + index}>
+            {item}
+            <button
+              onClick={() => {
+                data.items.splice(index, 1);
+                setData((data) => {
+                  return {
+                    ...data,
+                    items: data.items,
+                  };
+                });
+              }}
+            >
+              Remove Item
+            </button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        name="username"
+        placeholder="please insert a name"
+        value={data.itemsInput}
+        onChange={handleInputChange}
+      ></input>
+      <button onClick={handleAdd} disabled={!data.itemsInput}>
+        Add
+      </button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 }
+
+// export class TodoList extends React.Component {
+//   state = {
+//     items: ["Squirtle", "Charmander", "Bulbasaur"],
+//     itemsInput: "",
+//   };
+//   handlerAdd = () => {
+//     this.setState({
+//       items: [...this.state.items, this.state.itemsInput],
+//       itemsInput: "",
+//     });
+//   };
+//   handleInputChange = (event) => {
+//     this.setState({
+//       itemsInput: event.target.value,
+//     });
+//   };
+//   handleReset = () => {
+//     this.setState({
+//       items: [],
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <div>{this.props.render(this.state.items)}</div>
+//         <input
+//           type="text"
+//           name="username"
+//           placeholder="please insert a name"
+//           value={this.state.itemsInput}
+//           onChange={this.handleInputChange}
+//         ></input>
+//         <button onClick={this.handlerAdd} disabled={!this.state.itemsInput}>
+//           ADD
+//         </button>
+//         <button onClick={this.handleReset}>RESET</button>
+//       </div>
+//     );
+//   }
+// }
