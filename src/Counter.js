01305b07312c263fb90367662e598fbfc61ec57b
Martib01 <martybb01@gmail.com>
2022-08-05 18:29:45 +0200

    End useEffect-02

--- src/Counter.js
+++ src/Counter.js
@@ -1,42 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export class Counter extends React.Component {
-  state = {
-    count: this.props.initialValue,
-  };
-  // constructor(props) {
-  //   super(props);
-  //   setInterval(() => {
-  //     this.setState((state) => {
-  //       return {
-  //         count: state.count + this.props.incrementValue,
-  //       };
-  //     });
-  //   }, this.props.timeout);
-  // }
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tickTok(), this.props.timeout);
-  }
+export function Counter({
+  initialValue = 0,
+  incrementValue = 1,
+  timeout = 1000,
+}) {
+  const [count, setCount] = useState(initialValue);
 
-  tickTok() {
-    this.setState((state) => {
-      return {
-        count: state.count + this.props.incrementValue,
-      };
-    });
-  }
-  render() {
-    return (
-      <div>
-        <CounterDisplay count={this.state.count} />
-      </div>
-    );
-  }
+  useEffect(() => {
+    incrementCounter();
+
+    return () => {
+      clearInterval({ incrementValue });
+    };
+  });
+
+  const incrementCounter = () => {
+    setInterval(() => {
+      setCount((count) => count + incrementValue);
+    }, timeout);
+  };
+
+  return (
+    <div>
+      <CounterDisplay count={count} />
+    </div>
+  );
 }
 
-Counter.defaultProps = {
-  initialValue: 0,
-  incrementValue: 1,
-  timeout: 1000,
-};
+// export class Counter extends React.Component {
+//   state = {
+//     count: this.props.initialValue,
+//   };
+//   // constructor(props) {
+//   //   super(props);
+//   //   setInterval(() => {
+//   //     this.setState((state) => {
+//   //       return {
+//   //         count: state.count + this.props.incrementValue,
+//   //       };
+//   //     });
+//   //   }, this.props.timeout);
+//   // }
+//   componentDidMount() {
+//     this.timerID = setInterval(() => this.tickTok(), this.props.timeout);
+//   }
+
+//   tickTok() {
+//     this.setState((state) => {
+//       return {
+//         count: state.count + this.props.incrementValue,
+//       };
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <CounterDisplay count={this.state.count} />
+//       </div>
+//     );
+//   }
+// }
+
+// Counter.defaultProps = {
+//   initialValue: 0,
+//   incrementValue: 1,
+//   timeout: 1000,
+// };
