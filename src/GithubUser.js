7d2ad05c4cfd3b9d7bcaf9aa275f92412c8bc3d1
Martib01 <martybb01@gmail.com>
2022-08-07 13:02:52 +0200

    End custom hooks-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,29 +1,10 @@
-import { useEffect, useState } from "react";
+import { useGithubUser } from "./useGithubuser";
 
 export function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { data, loading, error, onFetchUser } = useGithubUser(username);
 
-  async function fetchGithubUser(username) {
-    setLoading(true);
-    setError(false);
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-    } finally {
-      setLoading(false);
-    }
-  }
-  useEffect(() => {
-    fetchGithubUser(username);
-  }, [username]);
   return (
-    <div>
+    <div onChange={onFetchUser}>
       {loading && <h1>Loading...</h1>}
       {error && <h1>An error has occurred</h1>}
       {data && <h1>{data.name}</h1>}
