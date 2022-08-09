6d50794e5475a85d38159c6f9954ceab97b82c6a
Martib01 <martybb01@gmail.com>
2022-08-09 12:58:57 +0200

    End useContext

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
 
-export class DisplayLanguage extends React.Component {
-  render() {
-    return (
-      <div>
-        <LanguageContext.Consumer>
-          {(language) => {
-            return <h1>The selected language is: {language}</h1>;
-          }}
-        </LanguageContext.Consumer>
-      </div>
-    );
-  }
+export function DisplayLanguage() {
+  const language = useContext(LanguageContext);
+
+  return <h3>Current Language: {language}</h3>;
 }
+
+// export class DisplayLanguage extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <LanguageContext.Consumer>
+//           {(language) => {
+//             return <h1>The selected language is: {language}</h1>;
+//           }}
+//         </LanguageContext.Consumer>
+//       </div>
+//     );
+//   }
+// }
