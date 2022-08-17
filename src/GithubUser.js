22306bb9d1252378cf31689279191e0ccf1856d1
Martib01 <martybb01@gmail.com>
2022-08-17 10:53:23 +0200

    end swr-01

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,10 +1,12 @@
+import { useParams } from "react-router-dom";
 import { useGithubUser } from "./useGithubuser";
 
-export function GithubUser({ username }) {
-  const { data, loading, error, onFetchUser } = useGithubUser(username);
+export function GithubUser() {
+  const { username } = useParams();
+  const { data, loading, error } = useGithubUser(username);
 
   return (
-    <div onChange={onFetchUser}>
+    <div>
       {loading && <h1>Loading...</h1>}
       {error && <h1>An error has occurred</h1>}
       {data && <h1>{data.name}</h1>}
